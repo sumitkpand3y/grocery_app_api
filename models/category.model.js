a9eeb18a7abebf0f6f48fc40e76e654fcd876919
Sumit Kumar Pandey <sumit.p@triofi.in>
2023-01-04 12:51:28 +0530

    first commit

---
+++ models/category.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const category  = mongoose.model(
+    "Category",
+    mongoose.Schema(
+        {
+            categoryName:{
+                type:String,
+                require:true,
+                unique:true,
+            },
+            categoryDescription:{
+                type:String,
+                require:false
+            },
+            categoryImage:{
+                type:String
+            }
+        }
+    )
+)
+
+module.exports = {
+    category,
+};
